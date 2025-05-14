3f87e0ab83f98ef84701780fb970d440647b34ea
defitas <syarifdefita@gmail.com>
2025-05-14 12:21:17 +0700

    pertemuan 11

---
+++ routes/web.php
@@ -0,0 +1,35 @@
+<?php
+
+// import java.io
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Link;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// System.out.println();
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('halo', function () {
+	return "<h2>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h2>";
+});
+
+Route::get('blog', function () {
+	return view('blog');
+});
+
+Route::get('hello', [Link::class, 'helloword']);
+
+Route::get('pertama', function () {
+	return view('pertama');
+});
