0bfaf095be156b743f0fefc13421eeb252d8f659
defitas <syarifdefita@gmail.com>
2025-05-21 12:35:39 +0700

    pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 // import java.io
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Link;
+use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\BlogController;
 
 /*
 |--------------------------------------------------------------------------
@@ -61,3 +63,15 @@ Route::get('tes', function () {
 Route::get('index', function () {
 	return view('index');
 });
+
+Route::get('dosen', [Link::class, 'index']);
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+Route::post('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+Route::get('formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
